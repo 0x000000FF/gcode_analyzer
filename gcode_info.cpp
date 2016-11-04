49a1e5c6775cf19a0fd57e4c42be52c51441eaec
0x000000FF <qqqoptimus@gmail.com>
2016-11-04 18:56:47 +0800

    optimization

--- gcode_info.cpp
+++ gcode_info.cpp
@@ -19,24 +19,32 @@ gcode_info::gcode_info()
 
 int gcode_info::analyzer(QString file)
 {
-    double max_x = 0,max_y = 0,max_z = 0;
-    double x = 0.0,y = 0.0,z = 0.0,e = 0.0,last_z = -0.1,last_e = 0.0,count_e = 0.0;
-    double f = 3600;
-    unsigned int t = 0;
-    bool valid_z = false;
-    bool abs = true;
-    QStringList s_x,s_y,s_z,s_e,s_f;
-    int layers = 0;
-    QStringList s_list;
-    QString str = "";
+    QFile out_gcode(file+"~");
+    if (!out_gcode.open(QIODevice::ReadWrite | QIODevice::Text))
+    {
+        return -1;
+    }
     this->f_gcode.setFileName(file);
     if ( !this->f_gcode.open(QIODevice::ReadOnly | QIODevice::Text))
     {
         qDebug() << "open file faild,source busy" ;
         return -1;
     }
-    qDebug()<<"load file success";
     this->gcode_stream.setDevice(&this->f_gcode);
+    QTextStream out_stream(&out_gcode);
+
+    double max_x = 0,max_y = 0,max_z = 0;
+    double x = 0.0,y = 0.0,z = 0.0,e = 0.0,last_z = -0.1,last_e = 0.0,count_e = 0.0;
+    double f = 3600;
+    unsigned g = 0;
+    unsigned int t = 0;
+    bool valid_z = false;
+    bool abs = true;
+    int layers = 0;
+    int i = 0,j = 0;
+    QString str = "";
+    qDebug()<<"load file success";
+
     for(str = this->gcode_stream.readLine();!this->gcode_stream.atEnd();str = this->gcode_stream.readLine())
     {
         if (str.length() < 2)
@@ -49,79 +57,60 @@ int gcode_info::analyzer(QString file)
         }
         else
         {
-            if (str.contains("G92 E0"))
+            qDebug()<<"gcode lline"<<str;
+            for (i = 0;i < str.size();i++)
             {
-                count_e += e;
-                e = 0;
-            }
-            else if (str.contains("G90"))
-            {
-                abs = true;
-            }
-            else if (str.contains("G91"))
-            {
-                abs = false;
-            }
-//            qDebug()<<"not skip \n";
-            s_list = str.split(' ');
-//            qDebug()<<s_list;
-            s_x = s_list.filter("X");
-            s_y = s_list.filter("Y");
-            s_z = s_list.filter("Z");
-            s_e = s_list.filter("E");
-            s_f = s_list.filter("F");
-//            qDebug()<<s_list;
-//            qDebug()<<s_x<<","<<s_y<<","<<s_z<<","<<s_e;
-            valid_z = false;
-            if (!s_x.empty())
-            {
-                valid_z = true;
-                x = s_x[0].midRef(1).toDouble();
-                max_x = max_x > x ? max_x : x;
-            }
-            if (!s_y.empty())
-            {
-                valid_z = true;
-                y = s_y[0].midRef(1).toDouble();
-                max_y = max_y > y ? max_y : y;
-            }
-            if (!s_f.empty())
-            {
-                valid_z = true;
-                t += (int)((e - last_e) / (f / 3600));
-                last_e = e;
-                f = s_f[0].midRef(1).toInt();
-            }
-            if (!s_e.empty())
-            {
-                if (abs)
-                {
-                    e = s_e[0].midRef(1).toDouble();
-                }
-                else
-                {
-                    e += s_e[0].midRef(1).toDouble();
+                switch (str.at(i).toLatin1()) {
+                case 'G':
+                    j = str.indexOf(' ',(i + 1));
+                    qDebug()<<str.mid((i + 1),(j - i -1));
+                    g = str.mid((i + 1),(j - i -1)).toInt();
+                    i = j;
+                    break;
+                case 'X':
+                    j = str.indexOf(' ',(i + 1));
+                    qDebug()<<str.mid((i + 1),(j - i -1));
+                    x = str.mid((i + 1),(j - i -1)).toDouble();
+                    i = j;
+                    break;
+                case 'Y':
+                    j = str.indexOf(' ',(i + 1));
+                    qDebug()<<str.mid((i + 1),(j - i -1));
+                    y = str.mid((i + 1),(j - i - 1)).toDouble();
+                    i = j;
+                    break;
+                case 'Z':
+                    j = str.indexOf(' ',(i + 1));
+                    qDebug()<<str.mid((i + 1),(j - i -1));
+                    z = str.mid((i + 1),(j - i - 1)).toDouble();
+                    i = j;
+                    break;
+                case 'E':
+                    j = str.indexOf(' ',(i + 1));
+                    qDebug()<<str.mid((i + 1),(j - i -1));
+                    e = str.mid((i + 1),(j - i - 1)).toDouble();
+                    i = j;
+                    break;
+                case 'F':
+                    j = str.indexOf(' ',(i + 1));
+                    qDebug()<<str.mid((i + 1),(j - i -1));
+                    f = str.mid((i + 1),(j - i - 1)).toDouble();
+                    i = j;
+                    break;
+
+                case ';':
+                    i = str.size();
+                default:
+                    break;
                 }
             }
-            if (!s_z.empty())
-            {
-                z = s_z[0].midRef(1).toDouble();
-                max_z = max_z > z ? max_z : z;
-                if (valid_z && z > last_z)
-                {
-                    layers++;
-                    qDebug()<<str;
-                }
-                 last_z = z;
-            }
-//            qDebug()<<"E";
-//            qDebug()<<x<<","<<y<<","<<z<<endl;
+            qDebug()<<x<<","<<y<<","<<z<<","<<e<<","<<f;
         }
         this->layers = layers;
     }
+    out_gcode.close();
     qDebug()<<layers;
     qDebug()<<t;
-    qDebug()<<max_x<<","<<max_y<<","<<max_z<<","<<e;
     qDebug()<<"done \n";
     return 0;
 }
