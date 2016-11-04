fe7730ad8bbe310066e5e8c5108e2fcc4631f19b
0x000000FF <qqqoptimus@gmail.com>
2016-11-04 15:56:34 +0800

    caculate print time

--- gcode_info.cpp
+++ gcode_info.cpp
@@ -20,8 +20,12 @@ gcode_info::gcode_info()
 int gcode_info::analyzer(QString file)
 {
     double max_x = 0,max_y = 0,max_z = 0;
-    double x = 0.0,y = 0.0,z = 0.0,e = 0.0,f = 0.0;
-    QStringList s_x,s_y,s_z,s_e;
+    double x = 0.0,y = 0.0,z = 0.0,e = 0.0,last_z = -0.1,last_e = 0.0,count_e = 0.0;
+    double f = 3600;
+    unsigned int t = 0;
+    bool valid_z = false;
+    bool abs = true;
+    QStringList s_x,s_y,s_z,s_e,s_f;
     int layers = 0;
     QStringList s_list;
     QString str = "";
@@ -45,6 +49,19 @@ int gcode_info::analyzer(QString file)
         }
         else
         {
+            if (str.contains("G92 E0"))
+            {
+                count_e += e;
+                e = 0;
+            }
+            else if (str.contains("G90"))
+            {
+                abs = true;
+            }
+            else if (str.contains("G91"))
+            {
+                abs = false;
+            }
 //            qDebug()<<"not skip \n";
             s_list = str.split(' ');
 //            qDebug()<<s_list;
@@ -52,30 +69,50 @@ int gcode_info::analyzer(QString file)
             s_y = s_list.filter("Y");
             s_z = s_list.filter("Z");
             s_e = s_list.filter("E");
+            s_f = s_list.filter("F");
 //            qDebug()<<s_list;
 //            qDebug()<<s_x<<","<<s_y<<","<<s_z<<","<<s_e;
+            valid_z = false;
             if (!s_x.empty())
             {
+                valid_z = true;
                 x = s_x[0].midRef(1).toDouble();
                 max_x = max_x > x ? max_x : x;
             }
-//            qDebug()<<"X";
             if (!s_y.empty())
             {
+                valid_z = true;
                 y = s_y[0].midRef(1).toDouble();
                 max_y = max_y > y ? max_y : y;
             }
-//            qDebug()<<"Y";
+            if (!s_f.empty())
+            {
+                valid_z = true;
+                t += (int)((e - last_e) / (f / 3600));
+                last_e = e;
+                f = s_f[0].midRef(1).toInt();
+            }
+            if (!s_e.empty())
+            {
+                if (abs)
+                {
+                    e = s_e[0].midRef(1).toDouble();
+                }
+                else
+                {
+                    e += s_e[0].midRef(1).toDouble();
+                }
+            }
             if (!s_z.empty())
             {
                 z = s_z[0].midRef(1).toDouble();
                 max_z = max_z > z ? max_z : z;
-                layers++;
-            }
-//            qDebug()<<"Z";
-            if (!s_e.empty())
-            {
-                e += s_e[0].midRef(1).toDouble();
+                if (valid_z && z > last_z)
+                {
+                    layers++;
+                    qDebug()<<str;
+                }
+                 last_z = z;
             }
 //            qDebug()<<"E";
 //            qDebug()<<x<<","<<y<<","<<z<<endl;
@@ -83,6 +120,7 @@ int gcode_info::analyzer(QString file)
         this->layers = layers;
     }
     qDebug()<<layers;
+    qDebug()<<t;
     qDebug()<<max_x<<","<<max_y<<","<<max_z<<","<<e;
     qDebug()<<"done \n";
     return 0;
